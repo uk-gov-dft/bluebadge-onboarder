57e73f43472a1ed1d00bf77b014d3ca5a82c8222
Andrew Rea <email@andrewrea.co.uk>
2018-09-26 11:24:39 +0000

    simplifying the start up script

--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -4,23 +4,6 @@ git clone git@github.com:uk-gov-dft/dev-env.git
 
 pushd dev-env
 
-bash load-modules.sh
-
-docker-compose kill
-docker-compose rm
-
-docker-compose build
-docker-compose up -d --no-color
-
-./wait_for_it.sh localhost:5432 
-./wait_for_it.sh localhost:8681:/manage/actuator/health
-./wait_for_it.sh localhost:8381:/manage/actuator/health
-./wait_for_it.sh localhost:8281:/manage/actuator/health
-./wait_for_it.sh localhost:8081:/manage/actuator/health
-./wait_for_it.sh localhost:8481:/manage/actuator/health
-./wait_for_it.sh localhost:8181:/manage/actuator/health
-./wait_for_it.sh localhost:8581:/manage/actuator/health
-
-psql -h localhost -U developer -d bb_dev -f ./scripts/db/setup-users.sql 
+./rebuild.sh all
 
 popd
