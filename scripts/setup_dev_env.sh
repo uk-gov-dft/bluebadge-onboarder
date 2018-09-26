6437e489e71cc6b81c1cfa32804a4fe58a8c6ae2
Andrew Rea <email@andrewrea.co.uk>
2018-09-26 11:21:06 +0000

    Need the migrations

--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -9,11 +9,17 @@ bash load-modules.sh
 docker-compose kill
 docker-compose rm
 
-docker-compose up -d postgresql
+docker-compose build
+docker-compose up -d --no-color
 
 ./wait_for_it.sh localhost:5432 
-
-sleep 5
+./wait_for_it.sh localhost:8681:/manage/actuator/health
+./wait_for_it.sh localhost:8381:/manage/actuator/health
+./wait_for_it.sh localhost:8281:/manage/actuator/health
+./wait_for_it.sh localhost:8081:/manage/actuator/health
+./wait_for_it.sh localhost:8481:/manage/actuator/health
+./wait_for_it.sh localhost:8181:/manage/actuator/health
+./wait_for_it.sh localhost:8581:/manage/actuator/health
 
 psql -h localhost -U developer -d bb_dev -f ./scripts/db/setup-users.sql 
 
